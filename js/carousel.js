a903827f7ed13a4657317e6c6189876af9a613ac
sakura.katsumata <03sa26kura@gmail.com>
2022-03-21 13:53:55 +0000

    footer以外はひととおり

--- js/carousel.js
+++ js/carousel.js
@@ -1,11 +1,37 @@
 // カルーセル
 
-$(document).ready(function(){
+/* $(document).ready(function(){
     $('.carousel').slick({
         autoplay: true,
         autoplaySpeed: 10000,
         arrows: false,
         dots: true,
         fade: true,
+    });
+}); */
+
+$(function() {
+    function sliderSetting() {
+
+        var width = $(window).width();
+
+        if (width <= 768) {
+            $('.carousel').not('.slick-initialized').slick({
+                autoplay: true,
+                autoplaySpeed: 10000,
+                arrows: false,
+                dots: true,
+                fade: true,
+            });
+        }
+        else {
+            $('.slide.slick-initialized').slick('unslick');
+        }
+    }
+
+    sliderSetting();
+
+    $(window).resize(function() {
+        sliderSetting();
     });
 });
