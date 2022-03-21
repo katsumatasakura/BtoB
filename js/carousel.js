1fa0911dd4f327bf8f7f2d8fcbc9089877e82e84
sakura.katsumata <03sa26kura@gmail.com>
2022-03-21 03:06:57 +0000

    footer途中

---
+++ js/carousel.js
@@ -0,0 +1,11 @@
+// カルーセル
+
+$(document).ready(function(){
+    $('.carousel').slick({
+        autoplay: true,
+        autoplaySpeed: 10000,
+        arrows: false,
+        dots: true,
+        fade: true,
+    });
+});
